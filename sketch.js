f07709d24499c23290b57357876d6231f18c74fe
naflav <55958629+naflav@users.noreply.github.com>
2019-10-29 23:15:36 +0100

    pizza mission

--- sketch.js
+++ sketch.js
@@ -1,11 +1,33 @@
-function preload(){
+var imgObj = null;
+var animate;
+
+function preload() {
   // put preload code here
 }
 
 function setup() {
+  noCanvas();
   // put setup code here
 }
 
 function draw() {
   // put drawing code here
 }
+
+function init() {
+  imgObj = document.getElementById('myPizzaman');
+  imgObj.style.position = 'relative';
+  imgObj.style.left = '0px';
+}
+
+function moveRight() {
+  imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
+  animate = setTimeout(moveRight, 20);
+}
+
+function stop() {
+  clearTimeout(animate);
+  imgObj.style.left = '0px';
+}
+
+window.onload = init;
