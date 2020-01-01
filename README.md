886adef9a031a99ea2a37dd867879878bbd9c5b1
naflav <55958629+naflav@users.noreply.github.com>
2020-01-01 20:20:06 +0100

    Assignment Re-evaluation

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# P5-empty-project
+# "PIZZA BOY 3000" - Dom Manipulation
+
+- Fixed overall sketch to fit requirements
+- Fixed images
+
 This repository is the starting point of the assignments given in the elective course [Creative Coding at Politecnico di Milano](https://www11.ceda.polimi.it/schedaincarico/schedaincarico/controller/scheda_pubblica/SchedaPublic.do?&evn_default=evento&c_classe=696598&__pj0=0&__pj1=3ed8420c42c849845b5caa3de626e8fc).
 Browse [this website](https://drawwithcode.github.io/) if you want to know more about it.
--- sketch.js
+++ sketch.js
@@ -1,33 +1,95 @@
-var imgObj = null;
-var animate;
+let x;
+let moving;
 
 function preload() {
-  // put preload code here
+  pizzaman = loadImage("pizzaman.png");
+  space = loadImage("space.png");
+  pizzaria = loadImage("pizzaria.png");
+  casa = loadImage("casa.png");
+  win = loadImage('win.png');
+  lost = loadImage('lost.png');
+  restart = createButton("RESTART");
+  restart.style("position:absolute; right:31vw; bottom:3vw; padding:1vw 3.5vw; font-size:1.9vw;");
+  restart.mousePressed(setup);
+
+
 }
 
 function setup() {
-  noCanvas();
-  // put setup code here
+  moving = 0;
+  createCanvas(windowWidth, windowHeight);
+  click = createButton("GO");
+  click.style("position:absolute; right:18vw; bottom:3vw; padding:1vw 3.5vw; font-size:1.9vw;");
+  clear = createButton("STOP");
+  clear.style("position:absolute; right:3vw; bottom:3vw; padding:1vw 3.5vw; font-size:1.9vw;");
+  click.mousePressed(moveRight);
+  clear.mousePressed(stop);
+  let s = 'This is the latest automated pizza model delivery available in the market:';
+  fill(50);
+  textAlign(CENTER, CENTER);
+  textSize(15);
+  textFont('Roboto Mono');
+  text(s, 350, 10, 600, 300);
+  let P = 'PIZZA BOY 3000';
+  textAlign(CENTER, CENTER);
+  fill(50);
+  textSize(15);
+  text(P, 600, 50, 100, 300);
+  let r = 'Press "GO" to activate his JOURNEY. BUT You have to press "STOP" as soon as he reaches the delivery place, so he can deliver the pizza...Or else he will keep the pizza to himself. ';
+  fill(50);
+  textSize(15);
+  textAlign(CENTER, CENTER);
+  text(r, 370, 120, 600, 300);
+
+
+
+
+
+  x = 410;
 }
 
 function draw() {
-  // put drawing code here
+
+  if (moving == 1) {
+    background('#FF6347');
+    x = x + 2;
+  } else if (moving == 0) {
+    x = x;
+    if (x > 800 && x < 1000) {
+      image(win, 500, 200, 400, 400);
+
+
+    }
+
+
+
+  }
+  image(space, 0, 0, 400, 100);
+  image(pizzaria, 150, 380, 275, 275);
+  image(casa, 800, 400, 275, 275);
+
+  click.mousePressed(moveRight);
+  clear.mousePressed(stop);
+  image(pizzaman, x, 510, 140, 140);
+
+  if (x > 1100) {
+
+    image(lost, 500, 200, 400, 400);
+  }
+
 }
 
 function init() {
-  imgObj = document.getElementById('myPizzaman');
-  imgObj.style.position = 'relative';
-  imgObj.style.left = '0px';
+  clear();
+  setup();
 }
 
 function moveRight() {
-  imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
-  animate = setTimeout(moveRight, 20);
+  background('#FF6347');
+  moving = 1;
+
 }
 
 function stop() {
-  clearTimeout(animate);
-  imgObj.style.left = '0px';
+  moving = 0;
 }
-
-window.onload = init;
